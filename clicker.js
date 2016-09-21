330e58e82ab6057013f64ecafe979692ed9b3289
Andres Espinoza <a_espinosa@live.com>
2016-09-21 01:56:47 -0400

    zzzzz

---
+++ clicker.js
@@ -0,0 +1,203 @@
+$(function(){
+	$("#a").click(function(event) {
+			event.preventDefault();
+			main.updateletter("a");
+	});
+	}
+);
+
+$(function(){
+	$("#b").click(function(event) {
+			event.preventDefault();
+			main.updateletter("b");
+	});
+	}
+);
+
+$(function(){
+	$("#c").click(function(event) {
+			event.preventDefault();
+			main.updateletter("c");
+	});
+	}
+);
+
+$(function(){
+	$("#d").click(function(event) {
+			event.preventDefault();
+			main.updateletter("d");
+	});
+	}
+);
+
+$(function(){
+	$("#e").click(function(event) {
+			event.preventDefault();
+			main.updateletter("e");
+	});
+	}
+);
+
+$(function(){
+	$("#f").click(function(event) {
+			event.preventDefault();
+			main.updateletter("f");
+	});
+	}
+);
+
+$(function(){
+	$("#g").click(function(event) {
+			event.preventDefault();
+			main.updateletter("g");
+	});
+	}
+);
+
+$(function(){
+	$("#h").click(function(event) {
+			event.preventDefault();
+			main.updateletter("h");
+	});
+	}
+);
+
+$(function(){
+	$("#i").click(function(event) {
+			event.preventDefault();
+			main.updateletter("i");
+	});
+	}
+);
+
+$(function(){
+	$("#j").click(function(event) {
+			event.preventDefault();
+			main.updateletter("j");
+	});
+	}
+);
+
+$(function(){
+	$("#k").click(function(event) {
+			event.preventDefault();
+			main.updateletter("k");
+	});
+	}
+);$(function(){
+	$("#l").click(function(event) {
+			event.preventDefault();
+			main.updateletter("l");
+	});
+	}
+);
+
+$(function(){
+	$("#m").click(function(event) {
+			event.preventDefault();
+			main.updateletter("m");
+	});
+	}
+);
+
+$(function(){
+	$("#n").click(function(event) {
+			event.preventDefault();
+			main.updateletter("n");
+	});
+	}
+);
+
+$(function(){
+	$("#o").click(function(event) {
+			event.preventDefault();
+			main.updateletter("o");
+	});
+	}
+);
+
+$(function(){
+	$("#p").click(function(event) {
+			event.preventDefault();
+			main.updateletter("p");
+	});
+	}
+);
+
+$(function(){
+	$("#q").click(function(event) {
+			event.preventDefault();
+			main.updateletter("q");
+	});
+	}
+);
+
+$(function(){
+	$("#r").click(function(event) {
+			event.preventDefault();
+			main.updateletter("r");
+	});
+	}
+);
+
+$(function(){
+	$("#s").click(function(event) {
+			event.preventDefault();
+			main.updateletter("s");
+	});
+	}
+);
+
+$(function(){
+	$("#t").click(function(event) {
+			event.preventDefault();
+			main.updateletter("t");
+	});
+	}
+);
+
+$(function(){
+	$("#u").click(function(event) {
+			event.preventDefault();
+			main.updateletter("u");
+	});
+	}
+);
+
+$(function(){
+	$("#v").click(function(event) {
+			event.preventDefault();
+			main.updateletter("v");
+	});
+	}
+);$(function(){
+	$("#w").click(function(event) {
+			event.preventDefault();
+			main.updateletter("w");
+	});
+	}
+);
+
+$(function(){
+	$("#x").click(function(event) {
+			event.preventDefault();
+			main.updateletter("x");
+	});
+	}
+);
+
+$(function(){
+	$("#y").click(function(event) {
+			event.preventDefault();
+			main.updateletter("y");
+	});
+	}
+);
+
+$(function(){
+	$("#z").click(function(event) {
+			event.preventDefault();
+			main.updateletter("z");
+	});
+	}
+);
--- script.js
+++ script.js
@@ -3,49 +3,49 @@ alert ("Js is WORKING!!")
 
 //myObjects
 main ={};
-main.wordArray = {};
-main.wordUArray = {};
+main.wordArray = [];
+main.wordUArray = [];
+main.lives  = 6;
 
-main.lives  =6;
-main.NumberInWordBank = Word.Length;
+main.NumberInwordList = 30;
 
-main.Word = "test";
+main.word = "i am working";
 main.wordU = "";
 
 //myFunctions
 
 main.pullWord = function (){
-  main.Word = Word.List[(math.random()*main.NumberInWordBank)];
+  main.word = words.List[(math.random()*main.NumberInwordList)];
 }
 
 main.SetUnderline = function (){
-min.pullWord();
-for (i=0; i<main.Word.Length; i++){
-  main.main.lives[i] = main.Word.charAt(i);
-  main.wordArray[i] = "-";
+main.pullWord();
+for (i=0; i<main.word.length; i++){
+  main.wordArray[i] = main.word.charAt(i);
+  main.wordUArray[i] = "_";
 }
+
 main.wordU = main.wordArray.join("");
 document.getElementById("word").innerHTML = main.wordU;
-document.getElementById("numLetters").innerHTML = main.Word.length;
+document.getElementById("letters").innerHTML = main.word.length;
 }
 
-main.UpdateLetter = function(){
-  main.Changes = 0;
-  for (i = 0; i< main.Word.length; i++)
-    main.wordArray[i] = main.Word.charAt(i);
-      if(main.Word.charAt(i) == letter){
-        main.wordUArray[i] == letter;
-        main.Changes += 1;
-      }
-    }
-
-      if(main.Changes < 1){
-        main.lives -=1;
-        document.getElementById("lives").innerHtml = main.lives;
-      }
+main.updateletter = function(letter){
+	main.changes = 0;
+	for(i=0; i < main.word.length; i++){
+		main.wordarray[i] = main.word.charAt(i);
+		if(main.word.charAt(i) == letter){
+		main.wordUArray[i] = letter;
+		main.changes += 1;
+		}
+	}
+	if(main.changes < 1){
+		main.lives -= 1;
+		document.getElementById("lives").innerHTML = main.lives;
+	}
 
 main.wordU = main.wordUArray.join("");
-document.getElementById("Word").innerHTML = main.wordU;
+document.getElementById("word").innerHTML = main.wordU;
 
 main.Word1 = main.wordArray.join("");
 main.Word2 = main.wordUArray.join('');
@@ -54,11 +54,12 @@ main.Word2 = main.wordUArray.join('');
     alert("You WON! YEYYYY!")
     window.location.reload();
   }
-  if(main.Lives > 1){
+  if(main.lives > 1){
       document.getElementById(WORD).inner.HTML == main.word1;
       alert("You are dead! Try Again ")
       window.location.reload();
   }
 
-min.pullWord();
-main.SetUnderline();
+  main.Pullword();
+  main.SetUnderline();
+}
